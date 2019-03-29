3d8e291255d4497ab86ca3367d7251e0315bb373
Eden <eden@spiderservices.com>
2019-03-29 17:01:24 +0300

    table with common functions file

--- fi_generator/src/common_functions.py
+++ fi_generator/src/common_functions.py
@@ -1,8 +1,22 @@
+from glob import glob
 import json
 import re
 import sys
 import argparse
+import os
+import requests
+import subprocess
+import time
 from docx import Document
+
+
+# Method check if string has Yes/No
+def checkYesNo(str):
+    if (str == "Yes" or str == "yes" or str == "No" or str == "no"):
+        return True
+    return False
+
+
 
 # Method return to Step description and question from string
 def fiStepDescriptionQuestion(str):
@@ -12,19 +26,20 @@ def fiStepDescriptionQuestion(str):
     arrDesQue = str.split('.')
 
     if('?' not in str):
-        objDes = {'htmlType':'fiStpDsc', 'txt' : str}
+        objDes = {'htmlType':'fiStpPrc', 'txt' : removeCharsFunc(str)}
         htmlDataObjAraay.append(objDes)
     else:
         newDes = ""
         for description in arrDesQue[:-1]:
             newDes += description+"."
-        objDes = {'htmlType':'fiStpDsc', 'txt' : newDes}
+        objDes = {'htmlType':'fiStpPrc', 'txt' : removeCharsFunc(newDes)}
         htmlDataObjAraay.append(objDes)
-        objQue = {'fiStpQst':'fiStpDsc', 'txt' : arrDesQue[-1]}
+        objQue = {'htmlType':'fiStpQst', 'txt' : removeCharsFunc(arrDesQue[-1])}
         htmlDataObjAraay.append(objQue)
 
     htmlDataObj['htmlData'] = htmlDataObjAraay
     return htmlDataObj
+
 
 
 # Method return description about FI flow
@@ -52,6 +67,7 @@ def fiMainDescription(Header_Name, Header_Description):
     return FI_Main_HTML_Obj
 
 
+
 # Method return remove characters
 def removeCharsFunc(str):
     removeCharacters = ['\n','\t','\u200e']
@@ -60,9 +76,55 @@ def removeCharsFunc(str):
     return removeRegexFunc(str)
 
 
+
 # Method return remove start characters with regex
 def removeRegexFunc(str):
     return re.sub(r'(^ |^[0-9]{2}[A-Z])', '', str)
+
+
+
+# Method return to Task description and question from string
+def fiTaskDescriptionQuestion(str):
+    htmlDataObj = {}
+    htmlDataObjAraay = []
+
+    arrDesQue = str.split('.')
+
+    if('?' not in str):
+        objDes = {'htmlType':'fiStpPrc', 'txt' : removeCharsFunc(str)}
+        htmlDataObjAraay.append(objDes)
+    else:
+        newDes = ""
+        for description in arrDesQue[:-1]:
+            newDes += description+"."
+        objDes = {'htmlType':'fiStpPrc', 'txt' : removeCharsFunc(newDes)}
+        htmlDataObjAraay.append(objDes)
+
+    htmlDataObj['htmlData'] = htmlDataObjAraay
+    return htmlDataObj
+
+
+# Method return Task Object to 'Y' (Yes option only)
+def fiTaskYes(str, yesOption, noOption):
+    yesObj = {}
+    arrDesQue = str.split('.')
+    newDes = ""
+
+    if('?' not in str):
+        newDes = str
+    else:
+        yesObj['msgRtIx'] = removeCharsFunc(arrDesQue[-1])
+        for description in arrDesQue[:-1]:
+            newDes += description+"."
+
+    yesObj['to'] = removeCharsFunc(newDes[newDes.find("(")+1:newDes.find(")")])
+    yesObj['rtN'] = removeCharsFunc(noOption)
+    yesObj['msgRt'] = "1"
+    yesObj['typ'] = "1"
+    yesObj['tskNm'] = removeCharsFunc(newDes[:newDes.find("(")])
+    yesObj['rtY'] = removeCharsFunc(yesOption)
+
+    return yesObj
 
 
 # Method return Task Object to 'Y' (Yes option only)
@@ -78,3 +140,45 @@ def fiTaskYes2(str, yesOption, noOption):
     yesObj['rtY'] = removeCharsFunc(yesOption)
 
     return yesObj
+
+
+
+
+def save_as_docx_win(path):
+    # Opening MS Word
+    word = win32.gencache.EnsureDispatch('Word.Application')
+    doc = word.Documents.Open(path)
+    doc.Activate ()
+
+    # Rename path with .docx
+    new_file_abs = os.path.abspath(path)
+    new_file_abs = re.sub(r'\.\w+$', '.docx', new_file_abs)
+
+    # Save and Close
+    word.ActiveDocument.SaveAs(
+        new_file_abs, FileFormat=constants.wdFormatXMLDocument
+    )
+    doc.Close(False)
+
+    return new_file_abs
+
+
+def save_as_docx_mac(path):
+    #print("path",path)
+    #SOFFICE_PATH='../../Office.app/Contents/MacOS/soffice'
+    SOFFICE_PATH="C:\Program Files\LibreOffice\program\soffice.exe"
+    subprocess.call([SOFFICE_PATH, '--headless', '--convert-to', 'docx', path])
+    f = open(path)
+    return os.path.basename(f.name).replace("doc","docx")
+
+
+
+def post_api_server(uri, jsondata):
+    # defining the api-endpoint
+    API_ENDPOINT = "http://127.0.0.1/api/figenerator/new/"
+
+    headers = {
+        'content-type' : "application/json",
+        'Authorization' : "Basic ZmlnZW5lcmF0b3I6QWExMjM0NTY="
+    }
+    r = requests.post(API_ENDPOINT+uri, data = jsondata, headers = headers)
--- fi_generator/src/ye_no.py
+++ fi_generator/src/ye_no.py
@@ -2,141 +2,17 @@ from glob import glob
 import json
 import re
 import sys
+import time
 import argparse
 import os
 import requests
 import subprocess
 from docx import Document
-import time
+import common_functions as cf
+
+
 #import win32com.client as win32
 #from win32com.client import constants
-
-# Method check if string has Yes/No
-
-def checkYesNo(str):
-    if (str == "Yes" or str == "yes" or str == "No" or str == "no"):
-        return True
-    return False
-
-# Method return remove characters
-def removeCharsFunc(str):
-    for rchar in removeCharacters:
-        str = str.replace(rchar,'')
-    return removeRegexFunc(str)
-
-# Method return remove start characters with regex
-def removeRegexFunc(str):
-    return re.sub(r'(^ |^[0-9]{2}[A-Z])', '', str)
-
-
-# Method return to Step description and question from string
-def fiStepDescriptionQuestion(str):
-    htmlDataObj = {}
-    htmlDataObjAraay = []
-
-    arrDesQue = str.split('.')
-
-    if('?' not in str):
-        objDes = {'htmlType':'fiStpPrc', 'txt' : removeCharsFunc(str)}
-        htmlDataObjAraay.append(objDes)
-    else:
-        newDes = ""
-        for description in arrDesQue[:-1]:
-            newDes += description+"."
-        objDes = {'htmlType':'fiStpPrc', 'txt' : removeCharsFunc(newDes)}
-        htmlDataObjAraay.append(objDes)
-        objQue = {'htmlType':'fiStpQst', 'txt' : removeCharsFunc(arrDesQue[-1])}
-        htmlDataObjAraay.append(objQue)
-
-    htmlDataObj['htmlData'] = htmlDataObjAraay
-    return htmlDataObj
-
-
-
-# Method return to Task description and question from string
-def fiTaskDescriptionQuestion(str):
-    htmlDataObj = {}
-    htmlDataObjAraay = []
-
-    arrDesQue = str.split('.')
-
-    if('?' not in str):
-        objDes = {'htmlType':'fiStpPrc', 'txt' : removeCharsFunc(str)}
-        htmlDataObjAraay.append(objDes)
-    else:
-        newDes = ""
-        for description in arrDesQue[:-1]:
-            newDes += description+"."
-        objDes = {'htmlType':'fiStpPrc', 'txt' : removeCharsFunc(newDes)}
-        htmlDataObjAraay.append(objDes)
-
-    htmlDataObj['htmlData'] = htmlDataObjAraay
-    return htmlDataObj
-
-
-
-# Method return Task Object to 'Y' (Yes option only)
-def fiTaskYes(str, yesOption, noOption):
-    yesObj = {}
-    arrDesQue = str.split('.')
-    newDes = ""
-
-    if('?' not in str):
-        newDes = str
-    else:
-        yesObj['msgRtIx'] = removeCharsFunc(arrDesQue[-1])
-        for description in arrDesQue[:-1]:
-            newDes += description+"."
-
-    yesObj['to'] = removeCharsFunc(newDes[newDes.find("(")+1:newDes.find(")")])
-    yesObj['rtN'] = removeCharsFunc(noOption)
-    yesObj['msgRt'] = "1"
-    yesObj['typ'] = "1"
-    yesObj['tskNm'] = removeCharsFunc(newDes[:newDes.find("(")])
-    yesObj['rtY'] = removeCharsFunc(yesOption)
-
-    return yesObj
-
-
-def save_as_docx_win(path):
-        # Opening MS Word
-        word = win32.gencache.EnsureDispatch('Word.Application')
-        doc = word.Documents.Open(path)
-        doc.Activate ()
-
-        # Rename path with .docx
-        new_file_abs = os.path.abspath(path)
-        new_file_abs = re.sub(r'\.\w+$', '.docx', new_file_abs)
-
-        # Save and Close
-        word.ActiveDocument.SaveAs(
-            new_file_abs, FileFormat=constants.wdFormatXMLDocument
-        )
-        doc.Close(False)
-
-        return new_file_abs
-
-
-def save_as_docx_mac(path):
-    #print("path",path)
-    #SOFFICE_PATH='../../Office.app/Contents/MacOS/soffice'
-    SOFFICE_PATH="C:\Program Files\LibreOffice\program\soffice.exe"
-    subprocess.call([SOFFICE_PATH, '--headless', '--convert-to', 'docx', path])
-    f = open(path)
-    return os.path.basename(f.name).replace("doc","docx")
-
-
-
-def post_api_server(uri, jsondata):
-    # defining the api-endpoint
-    API_ENDPOINT = "http://127.0.0.1/api/figenerator/new/"
-
-    headers = {
-        'content-type' : "application/json",
-        'Authorization' : "Basic ZmlnZW5lcmF0b3I6QWExMjM0NTY="
-    }
-    r = requests.post(API_ENDPOINT+uri, data = jsondata, headers = headers)
-
 
 
 
@@ -171,7 +47,7 @@ else:
     if (path[0].endswith('doc')):
        # if (os.name == 'posix'):
             print("use save_as_docx_mac")
-            pathUrl=[save_as_docx_mac(path[0])]
+            pathUrl=[cf.save_as_docx_mac(path[0])]
             f=open(pathUrl[0])
             f.close()
             document = Document(pathUrl)
@@ -216,7 +92,7 @@ for table in tables:
         FI_row = []
         for cell in row.cells:
             for paragraph in cell.paragraphs:
-                if not checkYesNo(paragraph.text):
+                if not cf.checkYesNo(paragraph.text):
                     # print(paragraph.text)
                     FI_row.append(paragraph.text)
                     if len(FI_row) == 3:
@@ -225,14 +101,14 @@ for table in tables:
                         newNumberObj['n'] = str(FI_Num)
                         if(('(' not in  FI_row[0]) and (')' not in  FI_row[0])):
                             # Step
-                            newNumberObj['Y'] = {'to' : removeCharsFunc(FI_row[1]), 'typ' : '0'}
-                            newNumberObj['N'] = {'to' : removeCharsFunc(FI_row[2]), 'typ' : '0'}
-                            newNumberObj['htmlObj'] = fiStepDescriptionQuestion(FI_row[0])
+                            newNumberObj['Y'] = {'to' : cf.removeCharsFunc(FI_row[1]), 'typ' : '0'}
+                            newNumberObj['N'] = {'to' : cf.removeCharsFunc(FI_row[2]), 'typ' : '0'}
+                            newNumberObj['htmlObj'] = cf.fiStepDescriptionQuestion(FI_row[0])
                         else:
                             # Task
-                            newNumberObj['Y'] = fiTaskYes(FI_row[0], FI_row[1], FI_row[2])
+                            newNumberObj['Y'] = cf.fiTaskYes(FI_row[0], FI_row[1], FI_row[2])
                             newNumberObj['N'] = { 'typ' : '4' }
-                            newNumberObj['htmlObj'] = fiTaskDescriptionQuestion(FI_row[0])
+                            newNumberObj['htmlObj'] = cf.fiTaskDescriptionQuestion(FI_row[0])
                         FI_Array.append(newNumberObj)
                         FI_Num+=1
 
@@ -248,5 +124,5 @@ jsonrequest = "{\"PG\":"+json.dumps(FI_Array, indent=4, sort_keys=True)+"}"
 f.write(jsonrequest)
 print(jsonrequest)
 
-post_api_server(glob(args.result, recursive=True), jsonrequest)
+cf.post_api_server(glob(args.result, recursive=True), jsonrequest)
 
